389f5900dffc3b6dd92e7e395554195f9970e7d8
Rumen Topalov <topalovr@gmail.com>
2018-01-28 10:09:30 +0200

    Laravel 5 Projects

--- ItBlogPost/itpostblog/resources/views/pages/contact.blade.php
+++ ItBlogPost/itpostblog/resources/views/pages/contact.blade.php
@@ -2,11 +2,11 @@
 @section('title', 'Contact')
 
 @section('content')
-        <h2>Contact</h2>
+       <h2>Contact</h2>
         <ul class="list-group">
-            <li class="list-group-item">Name<h4><span class="label label-primary">Rumen Topalov</span></h4></li>
-            <li class="list-group-item">Email <h4><span class="label label-primary">topalovr@gmail.com</span></h4></li>
-            <li class="list-group-item">Phone <h4><span class="label label-primary">+359 897 647465</span></h4></li>
+            <li style="font-weight: bold" class="list-group-item">Name<h4><span class="label label-primary">Rumen Topalov</span></h4></li>
+            <li style="font-weight: bold" class="list-group-item">Email <h4><span class="label label-primary">topalovr@gmail.com</span></h4></li>
+            <li style="font-weight: bold" class="list-group-item">Github <h4><span class="label label-primary"><a style="color: white" target="_blank" href="https://github.com/froddo">GitHub</a></span></h4></li>
+            <li style="font-weight: bold" class="list-group-item">Phone <h4><span class="label label-primary">+359 897 647465</span></h4></li>
         </ul>
-
 @endsection
